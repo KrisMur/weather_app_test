34cee6a49565c543c9e8133c5b0e4c85ab77938d
Kris Murvanidze <99450440+KrisMur@users.noreply.github.com>
2023-01-12 04:08:46 +0100

    change Router

--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import store from './redux/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter >
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
+git add src/.
